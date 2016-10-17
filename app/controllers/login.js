eff5657ff4446c78c45fd3e22397d937736b028f
skipsuva <skip.suva@gmail.com>
2016-10-17 17:27:24 -0400

    add ember-simple-auth foundation

---
+++ app/controllers/login.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+
+  actions: {
+    authenticate() {
+      let { email, password } = this.getProperties('email', 'password');
+      this.get('session').authenticate('authenticator:oauth2', email, password).catch((reason) => {
+        this.set('errorMessage', reason.error || reason);
+      });
+    }
+  }
+
+});
