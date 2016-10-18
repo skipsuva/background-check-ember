7264c037c3a62b11a84321c714906c81af2169e4
skipsuva <skip.suva@gmail.com>
2016-10-18 09:02:15 -0400

    add search template and email validation

---
+++ app/controllers/search.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    searchEmail(){
+      let query = this.get('emailQuery');
+      if(this.queryIsValid(query)){
+        this.set('errorMessage', "Wooooo!");
+      }else{
+        this.set('errorMessage', "Please enter a valid email address.");
+      }
+    }
+
+  },
+
+  queryIsValid(query){
+    let emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return emailRegex.test(query);
+  }
+});
