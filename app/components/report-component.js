9ea9ed2ab1c5f636d45b95d90c7153a20d91797e
skipsuva <skip.suva@gmail.com>
2016-10-19 09:08:29 -0400

    add landing page, image to report, some styling cleanup

--- app/components/report-component.js
+++ app/components/report-component.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  canShowImage: Ember.computed(function() {
+    let currentReport = this.get('model');
+    if(currentReport.images.length > 0){
+      return true;
+    }
+    return false;
+  }),
+  imageUrl: Ember.computed(function() {
+    let currentReport = this.get('model');
+    if(this.get('canShowImage')){
+      return currentReport.images[0].url;
+    }
+  }),
+
   actions: {
   }
 });
